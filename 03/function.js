bd78fe84c59db56b90337c68da189c50b77168fe
heebon00 <heebon21@gmail.com>
2026-07-09 12:24:49 +0900

    260709 업로드

--- 03/function.js
+++ 03/function.js
@@ -8,9 +8,26 @@ function 명명함수(params) {
 //2-1.표현식함수
 
 const 표현식함수 = function (m1,m2) {
-    document.write(`오늘 점심이란 없다:${m1}없다${m2}없어`
+    document.write(`오늘 점심이란 없다:${m1}없다${m2}없어<br>`
     )
 };
 표현식함수("없어","없다");
+표현식함수('라면',"김밥");
 //2-2.즉시실행함수
-//2-2.화살표함수
+ (function(a,b){
+    document.write(`<h1>즉시실행함수</h1><hr>${
+       a+b
+    }`);
+ })(1,2);
+//2-3.화살표함수
+//const 화살표함수 = function() {
+//onst 화살표함수 = () => { //1단 변신
+const 화살표함수 =()=>document.write(`화살표함수 <br>`);
+화살표함수();
+
+const sum = (a,b) =>{return a+b;
+console.log(a+b);
+return;
+};
+sum(50,10);
+
